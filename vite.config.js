fdad25bbc617be77e3a1f986d6b5fbc249474c3d
ayushi <ayushi.110718@gmail.com>
2026-07-28 11:31:05 +0530

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/expensetracker/',
+  base: '/Ledgerly-AI-based-Expense-Tracker/',
 })
